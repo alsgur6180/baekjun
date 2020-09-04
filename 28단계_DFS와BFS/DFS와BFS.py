d17ac558ecae3cfe838f363ce7f2d0d746118c7c
alsgur6180 <alsgur6180@naver.com>
2020-09-04 21:47:57 +0900

    DFS 구현 파이썬

--- 28단계_DFS와BFS/DFS와BFS.py
+++ 28단계_DFS와BFS/DFS와BFS.py
@@ -3,17 +3,12 @@ graph = [[0]*(n+1) for _ in range(n+1)]
 for i in range(m):
     x,y = map(int,input().split())
     graph[x][y] =1
-def dfs(x,y):
-    if x<=0 or x>=n or y<=0 or y>=n:
-        return 0
-    if graph[x][y] ==1:
-        graph[x][y] =0
-        print(y)
-        dfs(x-1,y)
-        dfs(x,y-1)
-        dfs(x+1,y)
-        dfs(x,y+1)
-        
-    return 0
-print(v)
-dfs(v,graph[v].index(1))
+    graph[y][x] =1
+def dfs(start,visited):
+    visited += [start]
+    for c in range(len(graph[start])):
+        if graph[start][c] == 1 and(c not in visited):
+            dfs(c,visited)
+    return visited
+print(dfs(v,[]))
+
